fa0a9383fb83450abd0b82e60251bf8148fd5572
edahmitchel <edahmitchel@gmail.com>
2023-08-21 09:36:35 +0100

    built game loop

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,18 @@ int main()
 {
 
     cout << "check start" << endl;
-
+    // game window dimensions
+    const int screen_width = 1200;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "pong game");
+    // speed.frame rate
+    SetTargetFPS(60);
+    // game loop
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
